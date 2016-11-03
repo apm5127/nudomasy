e0d851cd8211d3efc56fb510f148ea24ea24ab9d
apm5127 <apm5127@gmail.com>
2016-11-03 00:57:34 -0400

    Got server to work with built files in /dist

--- server/server.js
+++ server/server.js
@@ -1,5 +1,8 @@
 var express = require('express');
 var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
@@ -10,14 +13,15 @@ var port = 4300;
 var app = express();
 
 // View Engine
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, '../client/dist'));
 app.set('view engine', 'ejs');
 app.engine('html', require('ejs').renderFile);
 
 // Set Static Folder
-app.use(express.static(path.join(__dirname, `client`)));
+app.use(express.static(path.join(__dirname, `../client/dist`)));
 
 // Body Parser MW
+app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
 	extended: false
